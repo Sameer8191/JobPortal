4f79a37b7ec5c5c8542eacddfbbf054dbf3abba6
Sameer <shahfrnz001@gmail.com>
2026-09-25 07:24:34 +0530

    created helper util

---
+++ backend/controllers/interview.controller.js
@@ -0,0 +1,15 @@
+import InterviewCompany from "../models/interviewCompany.model.js";
+import InterviewQuestion from "../models/interviewQuestion.model.js"
+import interviewRole from "../models/interviewRole.model.js";
+import RoleQuestion from "../models/roleQuestion.model.js";
+
+// add to a company interview question
+
+const addQuestion = async(req,res)=>{
+  try {
+    
+    
+  } catch (error) {
+    
+  }
+}
